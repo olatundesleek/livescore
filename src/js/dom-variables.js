493e706acd02217279ccd2b03c43519f1bd0e4f5
chineduogada <chineduogada@gmail.com>
2019-09-30 18:13:11 +0100

    working out the modal

--- src/js/dom-variables.js
+++ src/js/dom-variables.js
@@ -1,5 +1,7 @@
 // header nav-bar
 const HEADER_NAV_BAR = document.querySelector("#header .nav-bar");
+// modal
+const MODAL = document.querySelector(".modal");
 
 // exporting all variables
-export { HEADER_NAV_BAR };
+export { HEADER_NAV_BAR, MODAL };
--- src/js/events/header-nav.js
+++ src/js/events/header-nav.js
@@ -1,9 +1,59 @@
-import { HEADER_NAV_BAR } from "../dom-variables.js";
+import { HEADER_NAV_BAR, MODAL } from "../dom-variables.js";
 
 export default class UI {
 	constructor() {
 		HEADER_NAV_BAR.addEventListener("click", event => {
-			console.log(event.target.classList.contains("nav-bar-search-icon"));
+			if (event.target.classList.contains("nav-bar-icon")) {
+				MODAL.classList.toggle("modal-toggle");
+				if (event.target.classList.contains("nav-bar-search-icon")) {
+					MODAL.innerHTML = `
+					<!-- search -->
+					<div class="search-bar">
+						<input class="search" type="text" placeholder="Enter your search here">
+						<a class="dropdown milli" href="#">All Sports&nbsp;<i class="fa fa-caret-down"></i></a>
+					</div>
+					<p class="search-instruction milli">Please Enter the minimum 3 characters. Results will start displaying here
+						immediately.</p>
+
+					<div class="table table-2">
+						<div class="table-name font-w-bold pad-half-left">Most popular searches
+						</div>
+
+						<div class="table-2-content">
+							<ul>
+								<li><a href="#">Spain: LaLiga</a></li>
+								<li><a href="#">England: Premier League</a> </li>
+								<li><a href="#">Juventus (italy)</a></li>
+								<li><a href="#">Ajax (Netherlands)</a></li>
+								<li><a href="#">Tottenham (England)</a></li>
+								<li><a href="#">England: Premier League</a> </li>
+								<li><a href="#">Juventus (italy)</a></li>
+								<li><a href="#">Ajax (Netherlands)</a></li>
+								<li><a href="#">Tottenham (England)</a></li>
+								<li><a href="#">England: Premier League</a> </li>
+								<li><a href="#">Juventus (italy)</a></li>
+								<li><a href="#">Ajax (Netherlands)</a></li>
+								<li><a href="#">Tottenham (England)</a></li>
+							</ul>
+						</div>
+					</div>
+					`;
+				} else if (event.target.classList.contains("nav-bar-user-icon")) {
+					MODAL.innerHTML = `
+					<!-- Login Form -->
+					<form>
+						<div class="form-title">
+							Login
+						</div>
+						<input class="username/email" type="text" placeholder="Username or Email...">
+						<input class="username/email" type="password" placeholder="Password...">
+
+						<button class="btn-dark">Login</button>
+						<button class="btn-dark">Register</button>
+					</form>
+					`;
+				}
+			}
 		});
 	}
 }
